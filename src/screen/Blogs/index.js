2f00643f454b0339a2ec26b6fb6772e15289466e
nayan10102 <nayansharma1010@gmail.com>
2022-07-16 13:17:05 +0530

    fixed api call

--- src/screen/Blogs/index.js
+++ src/screen/Blogs/index.js
@@ -1,6 +1,7 @@
 import {useEffect,useState,useRef} from 'react'
 import { useNavigate } from 'react-router-dom';
 import { makeRequest } from '../../service/yuri'
+import {Card,CardContent,Typography,Stack} from "@mui/material"
 import axios from 'axios';
 
 const Blogs = () => {
@@ -20,9 +21,23 @@ const Blogs = () => {
         },[])
 
     return(
-        <div>
-         {post.map((data) => <li>{data.name} , {data.country} , {data.domains.map((domains)=> {return domains})}</li>)}
-        </div>
+        <Stack direction="row" justifyContent="space-between" sx={{flexWrap:"wrap"}}>
+         {post.map((data) => 
+            <Card sx={{ width: 200, marginBottom:5 }}>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {data.name}
+              </Typography>
+              <Typography>
+                {data.country}
+              </Typography>
+              <Typography>
+                  {data.domains.map((domains)=> {return <div>{domains}</div>})}
+              </Typography>
+            </CardContent>
+          </Card>
+         )}
+        </Stack>
     )
 }
 
